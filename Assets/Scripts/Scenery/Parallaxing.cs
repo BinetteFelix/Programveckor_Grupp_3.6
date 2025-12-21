e95a24d7cad48b8584767c228706dceb683d9190
BinetteFelix <felix.binette@elev.ga.lbs.se>
2025-12-21 01:59:11 +0100

    Better parallaxing
    Changed the parallaxing script to better fit a commercial parallaxing system

--- Assets/Scripts/Scenery/Parallaxing.cs
+++ Assets/Scripts/Scenery/Parallaxing.cs
@@ -2,20 +2,28 @@ using UnityEngine;
 
 public class Parallaxing : MonoBehaviour
 {
-    private float startPos;
-    public GameObject cam;
-    public float distanceFromCamera;
+    public Camera cam;
+    public Transform player;
+  
+    private Vector2 startPos;
+    private Vector2 Travel => (Vector2)cam.transform.position - startPos;
+
+    private float startZPos;
+    private float DistanceFromPlayer => transform.position.z - player.position.z;
+    private float ClippingPlane => (cam.transform.position.z + (DistanceFromPlayer > 0 ? cam.farClipPlane : cam.nearClipPlane));
+    private float parallaxFactor => Mathf.Abs(DistanceFromPlayer) / ClippingPlane;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        startPos = transform.position;
+        startZPos = transform.position.z;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float distance = cam.transform.position.x * distanceFromCamera;
-        float movement = cam.transform.position.x * (1 - distanceFromCamera);
-
-        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+        Vector2 updatePosition = startPos + Travel * parallaxFactor;
+        transform.position = new Vector3(updatePosition.x, updatePosition.y, startZPos);
     }
 }
